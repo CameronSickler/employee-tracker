d0db82d14ae2c8f8f387147a51d90ab42eb72c04
Cameron <sicklecj@gmail.com>
2022-05-19 18:11:12 -0500

    set primary and foreign keys

--- schema.sql
+++ schema.sql
@@ -7,21 +7,26 @@ DROP TABLE IF EXISTS employee;
 -- create tables here
 
 CREATE TABLE department (
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL
+id INTEGER AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
+id INTEGER AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL,
-department_id INTEGER
+department_id INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
+id INTEGER AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INTEGER,
-manager_id INTEGER
+manager_id INTEGER,
+PRIMARY KEY (id, manager_id),
+FOREIGN KEY (roles_id) REFERENCES role(id)
 );
